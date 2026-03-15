ffc9fbed1b11755bf024fbb3e3234b2e4be16ed2
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2026-03-15 20:44:41 +0530

    Added functionality to fetch video ids list from playlist

--- video_status.py
+++ video_status.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id()-> str:
     try:
@@ -21,5 +22,34 @@ def get_playlist_id()-> str:
     except requests.exceptions.RequestException as e:
         raise e
     
+
+    
+def get_video_ids(playlist_id:str):
+
+    video_ids = []
+    pageToken=None
+    baseURL = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+        while True:
+            url = baseURL
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url=url)
+            data = response.json()
+
+            for item in data.get("items",[]):
+               id =  item["contentDetails"]["videoId"]
+               video_ids.append(id)
+
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    print(get_video_ids(playlist_id))
