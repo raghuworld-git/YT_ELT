1600ef1ab55eacaf391fd9e13a405fa8073888f0
Raghavendra <72355127+raghuworld-git@users.noreply.github.com>
2026-03-20 19:37:33 +0530

    Updated changes in dockerfile and and related changes

--- video_status.py
+++ video_status.py
@@ -8,7 +8,7 @@ import json
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
 
-CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
 maxResults = 50
 
 def get_playlist_id()-> str:
